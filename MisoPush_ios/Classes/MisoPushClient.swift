6ee1c9ad3a09a3ff889fff39162eb106624cec25
Hwan3434 <cookiegon3434@gmail.com>
2020-04-28 09:57:59 +0900

    objc add

--- MisoPush_ios/Classes/MisoPushClient.swift
+++ MisoPush_ios/Classes/MisoPushClient.swift
@@ -2,8 +2,12 @@
 
 import Foundation
 
+@objc
 public class MisoPushClient: NSObject {
+    
+    @objc
     public func log(with str: String){
         print("\(str) from CoocaPods");
     }
+    
 }
